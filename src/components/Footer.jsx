7beae9f7fd2c1e37a680614f409a86c903238cf9
J. Renner <j@lerenner.dev>
2021-04-15 13:53:56 -0300

    Add source code link; Change background color
    Signed-off-by: J. Renner <j@lerenner.dev>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer (props) {
       <div className="subFooterContainer">
         <hr />
         <p className="credits">Created by <a href="https://LeRenner.dev">LeRenner</a></p>
+        <a className="source" href="https://github.com/LeRenner/LePudimCalendar">Source Code</a>
       </div>
     </div>
   )
